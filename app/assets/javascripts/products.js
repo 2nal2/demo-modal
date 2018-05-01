eaa8a7220013ad2c2681aa94347fd48641b7680a
Donaldo Vargas <donaldov7@gmail.com>
2018-05-01 13:56:53 -0600

    subiendo

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,28 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+setCategory = function(id){
+    $("#categoryId").val(id);
+    $('#my-modal').modal('hide');
+}
+
+searchCategory = function(){
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
